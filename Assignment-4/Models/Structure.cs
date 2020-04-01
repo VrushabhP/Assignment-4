12d9e0ba8e06f45c9ce2f017e3b87b9a63815ede
VrushabhP <62633429+VrushabhP@users.noreply.github.com>
2020-03-31 22:44:48 -0400

    Add files via upload

---
+++ Assignment-4/Models/Structure.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CSV.Models
+{
+    public class Structure
+    {
+        //Set "MyRecord" parameter True for my record
+        public static void setMyRecord(Student stu)
+        {
+            Constants cons = new Constants();
+            if (stu.StudentId == cons.Student.StudentId)
+            {
+                stu.MyRecord = true;
+            }
+            else
+            {
+                stu.MyRecord = false;
+            }
+        }
+
+    }
+}
